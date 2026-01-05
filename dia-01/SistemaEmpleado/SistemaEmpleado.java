6ebef22fa196d0143990366a2c8322a18e9b4541
Andres Lopez <andres.lopezvivas@campusucc.edu.co>
2026-01-05 18:38:34 -0500

    Día 1: Ejercicio 1 - Sistema Estudiante completado

--- dia-01/SistemaEmpleado/SistemaEmpleado.java
+++
@@ -1,9 +0,0 @@
-public class SistemaEmpleado {
-    public static void main (String[] args){
-        String nombreEmpleado = "Felipe";
-        int horaTrabajadas = 160;
-        double PAGO_POR_HORA = 50000;
-        boolean tieneSeguroMedico = true;
-        
-    }
-}
